b157321f878db2bd8d0a8c6fb9803c071cf172a6
hrid1 <hridoycse1@gmail.com>
2024-10-31 13:23:31 +0600

    Project Setup Done

---
+++ src/pages/Country/Country.jsx
@@ -0,0 +1,9 @@
+const Country = () => {
+  return (
+    <div>
+      <h2>This is country page</h2>
+    </div>
+  );
+};
+
+export default Country;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+          <h3>This is homepage</h3>  
+        </div>
+    );
+};
+
+export default Home;
